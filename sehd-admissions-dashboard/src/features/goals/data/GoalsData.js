c55d769303a3d55e0fcaf9c7456da1481a291512
leonelSantos <leonel.adan.santos@gmail.com>
2025-04-09 11:42:58 -0400

    Update GoalsPage and GoalsData to reflect accurate enrollment targets and values for 2025

--- sehd-admissions-dashboard/src/features/goals/data/GoalsData.js
+++ sehd-admissions-dashboard/src/features/goals/data/GoalsData.js
@@ -4,64 +4,69 @@ const goalsData = {
         ALL: [
           {
             title: 'Community and Applied Psychological Studies, B.S.Ed. ',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'EPS'
           },
           {
             title: 'Data Analytics & Intelligence for Social Impact B.S.Ed. (DAISI)',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'EPS'
           },
           {
             title: 'Exercise Physiology, B.S.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'KIN'
           },
           {
             title: 'Sport Administration, B.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'KIN'
           },
           {
             title: 'Elementary Education/Exceptional Student Education, B.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'TAL'
           },
         ],
         KIN: [
           {
             title: 'Exercise Physiology, B.S.',
-            value: '421',
-            maxValue: '500'
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'KIN'
           },
           {
             title: 'Sport Administration, B.S.Ed.',
-            value: '421',
-            maxValue: '500'
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'KIN'
           }
         ],
         TAL: [
           {
             title: 'Elementary Education/Exceptional Student Education, B.S.Ed.',
-            value: '421',
-            maxValue: '500'
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'TAL'
           }
         ],
         EPS: [
           {
             title: 'Community and Applied Psychological Studies, B.S.Ed. ',
-            value: '421',
-            maxValue: '500'
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'EPS'
           },
           {
             title: 'Data Analytics & Intelligence for Social Impact B.S.Ed. (DAISI)',
-            value: '421',
-            maxValue: '500'
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'EPS'
           }
         ]
       },
@@ -71,87 +76,106 @@ const goalsData = {
         ALL: [
           {
             title: 'Higher Education Administration, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'EPS'
           },
           {
             title: 'Counseling, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: '14',
+            maxValue: '42',
             department: 'EPS'
           },
           {
             title: 'Applied Physiology, M.S.Ed. ',
-            value: '421',
-            maxValue: '500',
+            value: '12',
+            maxValue: '12',
             department: 'KIN'
           },
           {
             title: 'Athletic Training, M.S.AT.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'KIN'
           },
           {
             title: 'Sport Administration, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: '9',
+            maxValue: '30',
+            department: 'KIN'
+          },
+          {
+            title: 'Sport Administration, M.S.Ed.(Online)',
+            value: '1',
+            maxValue: '10',
             department: 'KIN'
           },
           {
             title: 'Education and Social Change, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: '0',
+            maxValue: '10',
             department: 'TAL'
           },
           {
             title: 'Special Education, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: '0',
+            maxValue: '10',
             department: 'TAL'
           }
         ],
         KIN: [
           {
             title: 'Applied Physiology, M.S.Ed. ',
-            value: '421',
-            maxValue: '500',
+            value: '12',
+            maxValue: '12',
+            department: 'KIN'
           },
           {
             title: 'Athletic Training, M.S.AT.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'KIN'
           },
           {
             title: 'Sport Administration, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: '9',
+            maxValue: '30',
+            department: 'KIN'
+          },
+          {
+            title: 'Sport Administration, M.S.Ed.(Online)',
+            value: '1',
+            maxValue: '10',
+            department: 'KIN'
           }
         ],
         TAL: [
           {
             title: 'Education and Social Change, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: '0',
+            maxValue: '10',
+            department: 'TAL'
           },
           {
             title: 'Special Education, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: '0',
+            maxValue: '10',
+            department: 'TAL'
           }
         ],
         EPS: [
           {
             title: 'Higher Education Administration, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'EPS'
           },
           {
             title: 'Counseling, M.S.Ed.',
-            value: '421',
-            maxValue: '500',
-          }
+            value: '14',
+            maxValue: '42',
+            department: 'EPS'
+          },
         ]
       },
       
@@ -160,75 +184,81 @@ const goalsData = {
         ALL: [
           {
             title: 'Counseling Psychology, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: '14',
+            maxValue: '2',
             department: 'EPS'
           },
           {
             title: 'Community Well-Being, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'EPS'
           },
           {
             title: 'Executive Ed.D. in Higher Education Leadership',
-            value: '421',
-            maxValue: '500',
+            value: '1',
+            maxValue: '12',
             department: 'EPS'
           },
           {
             title: 'Research, Measurement & Evaluation, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: '0',
+            maxValue: '1',
             department: 'EPS'
           },
           {
             title: 'Exercise Physiology, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'KIN'
           },
           {
             title: 'Teaching and Learning, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
             department: 'TAL'
           }
         ],
         KIN: [
           {
             title: 'Exercise Physiology, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'KIN'
           }
         ],
         TAL: [
           {
             title: 'Teaching and Learning, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'TAL'
           }
         ],
         EPS: [
           {
             title: 'Counseling Psychology, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: '14',
+            maxValue: '2',
+            department: 'EPS'
           },
           {
             title: 'Community Well-Being, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: 'N/A',
+            maxValue: 'N/A',
+            department: 'EPS'
           },
           {
             title: 'Executive Ed.D. in Higher Education Leadership',
-            value: '421',
-            maxValue: '500',
+            value: '1',
+            maxValue: '12',
+            department: 'EPS'
           },
           {
             title: 'Research, Measurement & Evaluation, Ph.D.',
-            value: '421',
-            maxValue: '500',
+            value: '0',
+            maxValue: '1',
+            department: 'EPS'
           }
         ]
       }
